5b758d78d31fea097081714e5b22e6028ede1e2f
Remarc Balisi <=>
2019-01-26 04:45:55 +0800

    display user list

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from 'src/app/services/token.service';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-user-list',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  users: any;
+  token: any;
+
+  constructor(
+    private apiService: ApiService,
+    private tokenService: TokenService
+  ) { }
 
   ngOnInit() {
+    this.token = this.tokenService.get();
+    this.apiService.getRequest('user-list', this.token).subscribe(response=>{
+      this.users = response;
+    });
   }
 
 }
